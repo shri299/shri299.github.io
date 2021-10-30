ae9ec1db7941efcd42edd5d6a9ad1fbf3d265943
srishti srivastava <ssrishti508@gmail.com>
2021-10-30 14:20:01 +0530

    Changed font

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 
-
-// scroll reveal
-
-// window.addEventListener('scroll',reveal);
-
-// function reveal(){
-//     var reveals = document.querySelectorAll('.reveal');
-
-//     for (let i = 0; i < reveals.length; i++) {
-        
-//         var windowHeight = window.innerHeight;
-//         var revealtop = reveals[i].getBoundingClientRect().top;
-//         var revealpoint = 100;
-//         if(revealtop<windowHeight-revealpoint)
-//         {
-//             reveals[i].classList.add('active');
-//         }
-//         else{
-//             reveals[i].classList.remove('active');
-//         }
-//  }
-// }
-
-
 //loader
+const about = document.querySelector(".second1");
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         document.getElementById("root").style.display = "block";
         document.querySelector(".loader").remove();
     }, 2000);
 })
+
+//scroll animation
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 100){
+        if(!about.classList.contains("reveal-content")){
+            about.classList.add("reveal-content");
+        }
+    }
+});
+
+
+
+
+
+
 
 
 
